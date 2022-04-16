67781c14d515d87e7f5e5621e6c3bc028581c7a0
Paulo <paulomafra2004@gmail.com>
2022-04-16 00:32:14 -0300

    Base do from criada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
-import NewProject from './components/pages/NewProjects';
+import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar'

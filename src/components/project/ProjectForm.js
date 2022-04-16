434dc91e38f739a8c7b5011e6ef98a3b6c5e93a1
Paulo <paulomafra2004@gmail.com>
2022-04-16 01:13:45 -0300

    Form concluído

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,20 +1,15 @@
-function projectForm(){
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css'
+
+function projectForm({btnText}){
     return(
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do Projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento do Projeto"/>
-            </div>
-            <div>
-                <select name="category_id">
-                <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto"/>
+            <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento do Projeto"/>
+            <Select name="category_id" text="Selecione a Categoria"/>
+            <SubmitButton text={btnText}/>
         </form>
     )
 }

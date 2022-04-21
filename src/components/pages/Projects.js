58f9f8f85c60ddbd18888802c1139b3d6422e9c8
Paulo <paulomafra2004@gmail.com>
2022-04-21 17:16:41 -0300

    Loading desenvolvido e adicionado

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,16 @@
 import styles from './Projects.module.css';
 import {useLocation} from 'react-router-dom'; 
+import { useState, useEffect } from 'react';
 import Message from "../layout/Message";
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard';
-import { useState, useEffect } from 'react';
+import Loading from '../layout/Loading';
 
 function Projects(){
     const [projects, setProjects] = useState([])
     const location = useLocation()
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     let message = ''
     if(location.state){
@@ -16,18 +18,20 @@ function Projects(){
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects',{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects',{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => console.log(err))
+        }, 500)
     }, [])
 
     return(
@@ -46,6 +50,10 @@ function Projects(){
                     category={project.category.name}
                     key={project.id}
                 />)}
+                {!removeLoading && <Loading/>}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
